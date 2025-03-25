73b3beaa0f8988876572139e42341d0c6436017d
RosyPetrella <petrellamr@gmail.com>
2025-03-25 14:41:48 +0100

    add languages array to ArrayList and import in App component

---
+++ src/ArrayList.jsx
@@ -0,0 +1,31 @@
+const languages = [
+  {
+    name: "HTML",
+    description: "Linguaggio di markup per la struttura delle pagine web.",
+  },
+  {
+    name: "CSS",
+    description: "Linguaggio per lo stile e il layout delle pagine web.",
+  },
+  {
+    name: "JavaScript",
+    description:
+      "Linguaggio di programmazione per rendere dinamiche le pagine web.",
+  },
+  {
+    name: "Node.JS",
+    description:
+      "Node.js è un runtime system open source multipiattaforma orientato agli eventi per l'esecuzione di codice JavaScript, costruito sul motore JavaScript V8 di Google Chrome.",
+  },
+  {
+    name: "Express",
+    description:
+      "Express.js è un framework minimalista per Node.js che semplifica la creazione di applicazioni web e API",
+  },
+  {
+    name: "ReactJS",
+    description:
+      "Libreria JavaScript per la creazione di interfacce utente interattive.",
+  },
+];
+export default languages;
