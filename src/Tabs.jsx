2680e1d95b2121951f93fdf3ac5456935c5e0424
RosyPetrella <petrellamr@gmail.com>
2025-03-25 15:33:39 +0100

    Edit Tabs component layout and styles; enhance tab content presentation

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -1,26 +1,29 @@
 export default function Tabs({ languages }) {
   return (
     <>
-      <ul className="nav nav-tabs">
-        {languages.map((lang, index) => (
-          <li key={index} className={index === 0 ? "active" : ""}>
-            <button data-toggle="tab" href="#home">
-              {lang.name}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="TabContainer">
+        <ul className="nav nav-tabs">
+          {languages.map((lang, index) => (
+            <li key={index} className={index === 0 ? "active" : ""}>
+              <button data-toggle="tab" href="#home">
+                {lang.name}
+              </button>
+            </li>
+          ))}
+        </ul>
 
-      <div className="tab-content">
-        {languages.map((lang, index) => (
-          <div
-            key={index}
-            id="home"
-            className={`tab-pane fade ${index === 0 ? "in active" : ""}`}
-          >
-            <p>{lang.description}</p>
-          </div>
-        ))}
+        <div className="TabContent">
+          {languages.map((lang, index) => (
+            <div
+              key={index}
+              id="home"
+              className={`tab-pane fade ${index === 0 ? "in active" : ""}`}
+            >
+              <h3>{lang.name}</h3>
+              <p>{lang.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
